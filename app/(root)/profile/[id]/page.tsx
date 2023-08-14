213e551bdf36a2f14079fe173ab775c8746b00a8
Lakshya Raikwal <lakshyaraikwal85@gmail.com>
2023-08-14 12:15:20 +0800

    Update page.tsx

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -40,12 +40,6 @@ async function Page({ params }: { params: { id: string } }) {
                   className="object-contain"
                 />
                 <p className="max-sm:hidden">{tab.label}</p>
-
-                {tab.label === "Strings" && (
-                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
-                    {userInfo?.threads?.length}
-                  </p>
-                )}
               </TabsTrigger>
             ))}
           </TabsList>
